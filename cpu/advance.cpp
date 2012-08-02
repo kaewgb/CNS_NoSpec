c9744bcb34f9111da11ea7466e56690503874d8e
Penporn Koanantakool <kaewgb@gmail.com>
2012-08-01 23:16:42 -0700

    [CPU] Removed multiple-box feature.

--- cpu/advance.cpp
+++ cpu/advance.cpp
@@ -4,7 +4,7 @@
 #include "helper_functions.h"
 
 void advance(
-	double ****U[],	// i/o
+	double ****U,	// i/o
 	double &dt,		// o
 	double dx[],	// i: dx[U.dim]
 	double cfl,		// i
@@ -13,8 +13,8 @@ void advance(
 ){
 	int lo[3], hi[3], i, j, k, l, n, nc, ng;
 	double courno, courno_proc;
-	double ****D[NBOXES], ****F[NBOXES], ****Unew[NBOXES], ****Q[NBOXES];
-	double ****Q2[NBOXES], ****D2[NBOXES], ****F2[NBOXES], ****Unew2[NBOXES], ****U2[NBOXES];
+	double ****D, ****F, ****Unew, ****Q;
+	double ****Q2, ****D2, ****F2, ****Unew2, ****U2;
 
     // Some arithmetic constants.
     double OneThird      = 1.E0/3.E0;
@@ -33,22 +33,20 @@ void advance(
 	hi[0] = hi[1] = hi[2] = NCELLS-1+NG;
 
 	// Allocation
-	FOR(i, 0, NBOXES){
-		allocate_4D(D[i], dim, nc);
-		allocate_4D(D2[i], dim, nc);
-		allocate_4D(F[i], dim, nc);
-		allocate_4D(F2[i], dim, nc);
-		allocate_4D(Q[i], dim_ng, nc+1);
-		allocate_4D(Q2[i], dim_ng, nc+1);
-		allocate_4D(Unew[i], dim_ng, nc);
-		allocate_4D(Unew2[i], dim_ng, nc);
-		allocate_4D(U2[i], dim_ng, nc);
-	}
+	allocate_4D(D, dim, nc);
+	allocate_4D(D2, dim, nc);
+	allocate_4D(F, dim, nc);
+	allocate_4D(F2, dim, nc);
+	allocate_4D(Q, dim_ng, nc+1);
+	allocate_4D(Q2, dim_ng, nc+1);
+	allocate_4D(Unew, dim_ng, nc);
+	allocate_4D(Unew2, dim_ng, nc);
+	allocate_4D(U2, dim_ng, nc);
 
 	//
 	// multifab_fill_boundary(U)
 	//
-	fill_boundary(U[0], dim, dim_ng);
+	fill_boundary(U, dim, dim_ng);
 
     //!
     //! Calculate primitive variables based on U.
@@ -56,8 +54,7 @@ void advance(
     //! Also calculate courno so we can set "dt".
     //!
 	courno_proc = 1.0E-50;
-	FOR(n, 0, NBOXES)
-		ctoprim(lo, hi, U[n], Q[n], dx, ng, courno_proc);
+	ctoprim(lo, hi, U, Q, dx, ng, courno_proc);
 
 	courno = courno_proc;
 	dt = cfl/courno;
@@ -66,25 +63,21 @@ void advance(
     //!
     //! Calculate D at time N.
     //!
-	FOR(n, 0, NBOXES)
-		diffterm(lo, hi, ng, dx, Q[n], D[n], eta, alam);
+	diffterm(lo, hi, ng, dx, Q, D, eta, alam);
 
     //!
     //! Calculate F at time N.
     //!
-	FOR(n, 0, NBOXES)
-		hypterm(lo, hi, ng, dx, U[n], Q[n], F[n]);
+	hypterm(lo, hi, ng, dx, U, Q, F);
 
     //!
     //! Calculate U at time N+1/3.
     //!
-	FOR(n, 0, NBOXES){
-		FOR(i, 0, dim[0]){
-			FOR(j, 0, dim[1]){
-				FOR(k, 0, dim[2]){
-					FOR(l, 0, nc)
-						Unew[n][i+NG][j+NG][k+NG][l] = U[n][i+NG][j+NG][k+NG][l] + dt*(D[n][i][j][k][l] + F[n][i][j][k][l]);
-				}
+	FOR(i, 0, dim[0]){
+		FOR(j, 0, dim[1]){
+			FOR(k, 0, dim[2]){
+				FOR(l, 0, nc)
+					Unew[i+NG][j+NG][k+NG][l] = U[i+NG][j+NG][k+NG][l] + dt*(D[i][j][k][l] + F[i][j][k][l]);
 			}
 		}
 	}
@@ -92,38 +85,33 @@ void advance(
 	//!
     //! Sync U^1/3 prior to calculating D & F. -- multifab_fill_boundary(Unew)
     //!
-	fill_boundary(Unew[0], dim, dim_ng);
+	fill_boundary(Unew, dim, dim_ng);
 
 	//!
     //! Calculate primitive variables based on U^1/3.
     //!
-    FOR(n, 0, NBOXES)
-		ctoprim(lo, hi, Unew[n], Q[n], dx, ng);
+	ctoprim(lo, hi, Unew, Q, dx, ng);
 
     //!
     //! Calculate D at time N+1/3.
     //!
-	FOR(n, 0, NBOXES)
-		diffterm(lo, hi, ng, dx, Q[n], D[n], eta, alam);
+	diffterm(lo, hi, ng, dx, Q, D, eta, alam);
 
 	//!
     //! Calculate F at time N+1/3.
     //!
-    FOR(n, 0, NBOXES)
-		hypterm(lo, hi, ng, dx, Unew[n], Q[n], F[n]);
+	hypterm(lo, hi, ng, dx, Unew, Q, F);
 
 	//!
     //! Calculate U at time N+2/3.
     //!
-	FOR(n, 0, NBOXES){
-		FOR(i, 0, dim[0]){
-			FOR(j, 0, dim[0]){
-				FOR(k, 0, dim[0]){
-					FOR(l, 0, nc)
-						Unew[n][i+NG][j+NG][k+NG][l] =
-							ThreeQuarters *  U[n][i+NG][j+NG][k+NG][l] +
-							OneQuarter    * (Unew[n][i+NG][j+NG][k+NG][l] + dt*(D[n][i][j][k][l] + F[n][i][j][k][l]));
-				}
+	FOR(i, 0, dim[0]){
+		FOR(j, 0, dim[0]){
+			FOR(k, 0, dim[0]){
+				FOR(l, 0, nc)
+					Unew[i+NG][j+NG][k+NG][l] =
+						ThreeQuarters *  U[i+NG][j+NG][k+NG][l] +
+						OneQuarter    * (Unew[i+NG][j+NG][k+NG][l] + dt*(D[i][j][k][l] + F[i][j][k][l]));
 			}
 		}
 	}
@@ -131,78 +119,68 @@ void advance(
 	//!
     //! Sync U^2/3 prior to calculating D & F. -- multifab_fill_boundary(Unew)
     //!
-	fill_boundary(Unew[0], dim, dim_ng);
+	fill_boundary(Unew, dim, dim_ng);
 
     //!
     //! Calculate primitive variables based on U^2/3.
     //!
-    FOR(n, 0, NBOXES)
-        ctoprim(lo, hi, Unew[n], Q[n], dx, ng);
+	ctoprim(lo, hi, Unew, Q, dx, ng);
 
     //!
     //! Calculate D at time N+2/3.
     //!
-    FOR(n, 0, NBOXES)
-        diffterm(lo, hi, ng, dx, Q[n], D[n], eta, alam);
+    diffterm(lo, hi, ng, dx, Q, D, eta, alam);
 
     //!
     //! Calculate F at time N+2/3.
     //!
-    FOR(n, 0, NBOXES)
-        hypterm(lo, hi, ng, dx, Unew[n], Q[n], F[n]);
+	hypterm(lo, hi, ng, dx, Unew, Q, F);
 
     //!
     //! Calculate U at time N+1.
     //!
-    FOR(n, 0, NBOXES){
-		FOR(i, 0, dim[0]){
-			FOR(j, 0, dim[0]){
-				FOR(k, 0, dim[0]){
-					FOR(l, 0, nc)
-						U[n][i+NG][j+NG][k+NG][l] =
-							OneThird    *  U[n][i+NG][j+NG][k+NG][l] +
-							TwoThirds   * (Unew[n][i+NG][j+NG][k+NG][l] + dt*(D[n][i][j][k][l] + F[n][i][j][k][l]));
-				}
+	FOR(i, 0, dim[0]){
+		FOR(j, 0, dim[0]){
+			FOR(k, 0, dim[0]){
+				FOR(l, 0, nc)
+					U[i+NG][j+NG][k+NG][l] =
+						OneThird    *  U[i+NG][j+NG][k+NG][l] +
+						TwoThirds   * (Unew[i+NG][j+NG][k+NG][l] + dt*(D[i][j][k][l] + F[i][j][k][l]));
 			}
 		}
 	}
 
 	// Free memory
-	FOR(i, 0, NBOXES){
-		free_4D(D[i], dim);
-		free_4D(D2[i], dim);
-		free_4D(F[i], dim);
-		free_4D(F2[i], dim);
-		free_4D(Q[i], dim_ng);
-		free_4D(Q2[i], dim_ng);
-		free_4D(Unew[i], dim_ng);
-		free_4D(Unew2[i], dim_ng);
-		free_4D(U2[i], dim_ng);
-	}
+	free_4D(D, dim);
+	free_4D(D2, dim);
+	free_4D(F, dim);
+	free_4D(F2, dim);
+	free_4D(Q, dim_ng);
+	free_4D(Q2, dim_ng);
+	free_4D(Unew, dim_ng);
+	free_4D(Unew2, dim_ng);
+	free_4D(U2, dim_ng);
 }
 
 void advance_test(){
 	int i, l, n;
 	int nc, dim_ng[3];
 	double dt, dt2, dx[DIM], cfl, eta, alam;
-	double ****U[NBOXES], ****U2[NBOXES];
+	double ****U, ****U2;
 	FILE *fin, *fout;
 
 	nc = NC;
 	dim_ng[0] = dim_ng[1] = dim_ng[2] = NCELLS+NG+NG;
 
 	// Allocation
-	FOR(i, 0, NBOXES){
-		allocate_4D(U[i], dim_ng, nc);
-		allocate_4D(U2[i], dim_ng, nc);
-	}
+	allocate_4D(U, dim_ng, nc);
+	allocate_4D(U2, dim_ng, nc);
 
 	// Initiation
 	fin = fopen("../fortran90/advance_input", "r");
-	FOR(n, 0, NBOXES){
-		FOR(l, 0, nc)
-			read_3D(fin, U[n], dim_ng, l);
-	}
+	FOR(l, 0, nc)
+		read_3D(fin, U, dim_ng, l);
+
 	fscanf(fin, "%le", &dt);
 	FOR(i, 0, 3)
 		fscanf(fin, "%le", &dx[i]);
@@ -214,20 +192,16 @@ void advance_test(){
 	advance(U, dt, dx, cfl, eta, alam);
 
 	fout=fopen("../fortran90/advance_output", "r");
-	FOR(n, 0, NBOXES){
-		FOR(l, 0, nc)
-			read_3D(fout, U2[n], dim_ng, l);
-		check_4D_array("U", U[n], U2[n], dim_ng, nc);
+	FOR(l, 0, nc)
+		read_3D(fout, U2, dim_ng, l);
+	check_4D_array("U", U, U2, dim_ng, nc);
 
-	}
 	fscanf(fout, "%le", &dt2);
 	check_double(dt, dt2, "dt");
 	fclose(fout);
 	printf("Correct!\n");
 
 	// Free memory
-	FOR(i, 0, NBOXES){
-		free_4D(U[i], dim_ng);
-		free_4D(U2[i], dim_ng);
-	}
+	free_4D(U, dim_ng);
+	free_4D(U2, dim_ng);
 }
--- cpu/header.h
+++ cpu/header.h
@@ -69,7 +69,7 @@ extern void ctoprim (
     int ng,         // i
     double &courno  // i/o
 );
-void ctoprim (
+extern void ctoprim (
     int lo[],       // i: lo[3]
     int hi[],       // i: hi[3]
     double ****u,   // i: u[hi[0]-lo[0]+2*ng][hi[1]-lo[1]+2*ng][hi[2]-lo[2]+2*ng][5]
@@ -103,7 +103,7 @@ extern void diffterm (
 
 extern void advance_test();
 extern void advance(
-	double ****U[],	// i/o
+	double ****U,	// i/o
 	double &dt,		// o
 	double dx[],	// i: dx[U.dim]
 	double cfl,		// i
