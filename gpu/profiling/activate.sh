fa9b23388af5cef6cb9a6e76ae861155c53eb768
Penporn Koanantakool <kaewgb@gmail.com>
2012-11-04 20:41:18 -0800

    Added profiling scripts.

---
+++ gpu/profiling/activate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export COMPUTE_PROFILE=1
+export COMPUTE_PROFILE_CSV=1
+export COMPUTE_PROFILE_CONFIG=/global/homes/p/penpornk/cns_nospec/gpu/profiling/full.conf
---
+++ gpu/profiling/deactivate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export COMPUTE_PROFILE=0
