8b012d3251c45c4f116f5c36b36e0ecfef7e15e4
Penporn Koanantakool <kaewgb@gmail.com>
2012-12-06 18:26:12 -0800

    Added the forgotten util.cuh. Solved the mystery 'main.o: target not found' make problem on Maeka.

---
+++ gpu/util.cuh
@@ -0,0 +1,15 @@
+#ifndef UTIL_CUH_INCLUDED
+#define	UTIL_CUH_INCLUDED
+
+__device__ double atomicAdd(double *address, double val){
+	unsigned long long int *address_as_ull = (unsigned long long int *) address;
+	unsigned long long int old = *address_as_ull, assumed;
+	do {
+		assumed = old;
+		old = atomicCAS( address_as_ull, assumed,
+						  __double_as_longlong(val + __longlong_as_double(assumed)));
+	} while(assumed != old);
+	return __longlong_as_double(old);
+}
+
+#endif
